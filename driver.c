5bba481304a65cd6b5dca31d2c1b96fcbbc1da6a
the moo <93696248+cowboyfn@users.noreply.github.com>
2021-11-05 00:02:56 -0400

    Create driver.c

---
+++ driver.c
@@ -0,0 +1,13 @@
+#include "diagonal.h"
+
+int main(int argc, char* argv[])
+{
+  FILE * in = fopen(argv[1], "r");
+  assert(in != NULL);
+  int sz = 0;
+  int **data = readData(in, &sz);
+  assert(data != NULL);
+  calculateDiagonal(sz, data);
+
+  return 0;
+}
